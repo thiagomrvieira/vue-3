9a4ae6483e279f5ec98676e8597e64f6c5d127ef
Thiago Vieira <tmaciel@digital-works.com>
2022-05-04 21:49:33 +0100

    One vue component per file

---
+++ js/components/AppButton.js
@@ -0,0 +1,17 @@
+export default {
+    template: `
+        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing" @click="process">
+            <slot />
+        </button>
+    `,
+    data() {
+        return {
+            processing: false,
+        }
+    },
+    methods: {
+        process() {
+            this.processing = !this.processing
+        }
+    },
+}
