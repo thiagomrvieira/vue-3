0a3889dbdb7d335a7ac0b084c5b78664de04b9d5
Thiago Vieira <tmaciel@digital-works.com>
2022-05-08 10:55:55 +0100

    Component porps

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,14 +1,38 @@
 export default {
     template: `
-        <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing" @click="process">
+        <button 
+            :class="{
+                'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+                'bg-blue-600 hover:bg-blue-700 ': type === 'primary',
+                'bg-purple-200 hover:bg-purple-400 ': type === 'secondary',
+                'bg-gray-200 hover:bg-gray-400 ': type === 'muted',
+                'is-loading': processing,
+            }" 
+            :disabled="processing" 
+            @click="process"
+        >
             <slot />
         </button>
     `,
-    data() {
-        return {
-            processing: false,
-        }
+
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+
+        processing : {
+            type: Boolean,
+            default: false,
+        },
     },
+
+    // data() {
+    //     return {
+    //         processing: false,
+    //     }
+    // },
+
     methods: {
         process() {
             this.processing = !this.processing
